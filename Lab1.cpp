8d4d7666687151fea0db50b64c0523eaadbba659
faaant <71407013+faaant@users.noreply.github.com>
2020-10-20 22:59:39 +0300

    Update Lab1.cpp
    Add comments

--- Lab1.cpp
+++ Lab1.cpp
@@ -6,9 +6,9 @@ int main()
 {
 double s,s1,n;
 
-cout << "Area of described square s = "; cin>>s; // Вводимо площу описаного квадрата
-while (s<=0) {cout <<"\nEnter the correct area s." <<"\n" <<"s = "; cin>>s;} //Перевірка коректності площі
-s1 = s/2; //Знаходимо площу вписаного квадрата
+cout << "Area of described square s = "; cin>>s; // Enter area of described square
+while (s<=0) {cout <<"\nEnter the correct area s." <<"\n" <<"s = "; cin>>s;} //Check data
+s1 = s/2; //search for the area of the inscribed square
 n = s/s1;
 cout<<"The area of an inscribed square " <<s1 <<"\n" <<"\n";
 cout<<"The area of the inscribed square is smaller than area described square in "<<n  <<" times" <<"\n";
