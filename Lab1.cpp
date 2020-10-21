248a512b31d344649f6f8d6e408c49d04f291b3c
Illia <illiaeducation@gmail.com>
2020-10-21 15:34:47 +0300

    Edited

--- Lab1.cpp
+++ Lab1.cpp
@@ -6,8 +6,7 @@ int main()
 {
 double s,s1,n;
 
-cout << "Area of described square s = "; cin>>s; // Вводимо площу описаного квадрата
-while (s<=0) {cout <<"\nEnter the correct area s." <<"\n" <<"s = "; cin>>s;} //Перевірка коректності площі
+cout << "Area of described square. The number must be >0. \ns = "; cin>>s; // Вводимо площу описаного квадрата
 s1 = s/2; //Знаходимо площу вписаного квадрата
 n = s/s1;
 cout<<"The area of an inscribed square " <<s1 <<"\n" <<"\n";
